5d03ed890a2f8d1ef5aab08f905f8273b5ef2d9b
Nikolas  Haug <nikolashaug@er-10-129-108-239.wireless.umn.edu>
2018-07-21 12:33:06 -0500

    fix models

---
+++ models/Article.js
@@ -0,0 +1,34 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// make the new article schema
+var ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    time: {
+        type: Date,
+        default: Date.now
+    },
+    comment: [{
+        type: Schema.Types.ObjectId,
+        ref: "UserComment"
+    }]
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
